38c95307957136313856184c2effb89f77992664
David Sullivan <david_sullivan@outlook.com>
2024-08-24 12:45:42 -0300

    Implementando função de Leitura de CSV

--- Aula07/funcoes.py
+++ Aula07/funcoes.py
@@ -1,13 +1,27 @@
+import csv
 
 
-lista = [1,2,3,4]
 
+"""Cria Função de Média de Valores"""
 def somaValoresLista(lista: list):
     return sum(lista)/len(lista)
 
 
-media = somaValoresLista(lista)
-
-print(media)
 
 
+"""Cria Função de Leitura de CSV e retorna uma lista de dicionarios"""
+def lerCsv(nomeArquivo: str) -> list[dict]:
+    listaItens = [];
+    with open(nomeArquivo, mode='r', encoding='utf-8') as arquivo:
+        leituraArquivo = csv.DictReader(arquivo, delimiter=';')
+        for itens in leituraArquivo:
+            listaItens.append(itens)
+    return listaItens
+
+
+
+
+pathArquivo = 'vendas.csv'
+arquivoVendas: list[dict]
+arquivoVendas = lerCsv(pathArquivo)
+print(arquivoVendas)
