898994f19cb895880a771c50fc7cf112c66a7c35
David Sullivan <david_sullivan@outlook.com>
2024-08-26 07:53:13 -0300

    Definição de função de Extração de Dados

--- Aula08/Etl.py
+++ Aula08/Etl.py
@@ -1,1 +1,16 @@
- 
+import pandas as pd
+import os 
+import glob
+
+pasta = 'data'
+
+
+def extrair_dados(path: str) -> pd.DataFrame:
+    arquivos_json = glob.glob(os.path.join(pasta, '*.json'))
+    df_list = [pd.read_json(arquivo) for arquivo in arquivos_json]
+    df_total = pd.concat(df_list, ignore_index=True)
+    return df_total
+
+
+print(extrair_dados(path=pasta))
+
