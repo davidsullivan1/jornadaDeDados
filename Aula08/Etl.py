d2a5e6910d86f177a6c40ce804e39699ce4c3eb6
David Sullivan <david_sullivan@outlook.com>
2024-08-31 09:44:27 -0300

    Criando ETL

--- Aula08/Etl.py
+++ Aula08/Etl.py
@@ -2,15 +2,35 @@ import pandas as pd
 import os 
 import glob
 
-pasta = 'data'
-
-
+#Lê e consolida arquivos json - retornando um Dataframe
 def extrair_dados(path: str) -> pd.DataFrame:
-    arquivos_json = glob.glob(os.path.join(pasta, '*.json'))
+    arquivos_json = glob.glob(os.path.join(path, '*.json'))
     df_list = [pd.read_json(arquivo) for arquivo in arquivos_json]
-    df_total = pd.concat(df_list, ignore_index=True)
+    df_total = pd.concat(df_list, ignore_index=True) #ignore_index = True, cria novos índices para a lista completa
     return df_total
 
 
-print(extrair_dados(path=pasta))
+
+def calcula_kpi_total_vendas(df: pd.DataFrame) -> pd.DataFrame:
+    df["Total"] = df["Quantidade"] * df["Venda"]
+    return df
+
+
+def load_dados(df: pd.DataFrame, format_saida: list):
+    for formato in format_saida:
+        if formato == 'csv':
+            df.to_csv("dados_consolidados.csv")
+        if formato == 'parquet':
+            df.to_csv("dados_consolidados.parquet")
+
+
+
+
+
+if __name__ == "__main__":
+    pasta = "data"
+    listaVendas = extrair_dados(path=pasta)
+    totalVendas = calcula_kpi_total_vendas(listaVendas)
+    formatoArquivos: list = ['csv', 'parquet']
+    load_dados(totalVendas, formatoArquivos)
 
